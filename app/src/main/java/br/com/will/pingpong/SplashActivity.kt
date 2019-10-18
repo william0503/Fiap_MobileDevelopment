c41133540e97bfe5a797b67307605afa1c85c12a
Logon Aluno <logonaluno@local.com>
2019-10-17 21:45:31 -0300

    Adicionando tela de splash v2

--- app/src/main/java/br/com/will/pingpong/SplashActivity.kt
+++ app/src/main/java/br/com/will/pingpong/SplashActivity.kt
@@ -1,12 +1,25 @@
 package br.com.will.pingpong
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            nextScreen()
+        }, 5000L)
+    }
+
+    private fun nextScreen() {
+        val nextScreen = Intent(this, MainActivity::class.java)
+        startActivity(nextScreen)
+        finish()
     }
 }
